c8e1f8ad70ff95702121b7ef4730d58ffcdd686f
Ogunbiyi Elijah <115193430+Akins20@users.noreply.github.com>
2025-07-08 10:25:11 +0000

    updated jwt auth period

--- video-meet-backend/src/config/index.ts
+++ video-meet-backend/src/config/index.ts
@@ -153,7 +153,7 @@ const config: Config = {
   jwt: {
     secret: process.env.JWT_SECRET || '',
     refreshSecret: process.env.JWT_REFRESH_SECRET || '',
-    expiresIn: process.env.JWT_EXPIRE || '15m',
+    expiresIn: process.env.JWT_EXPIRE || '7d',
     refreshExpiresIn: process.env.JWT_REFRESH_EXPIRE || '7d',
   },
   
